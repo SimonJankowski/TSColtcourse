73e8b79cf40935c955ece0930d7d1575a4f03581
SimonJay <szyszkakontrast@gmail.com>
2022-07-18 11:06:52 +0200

    update
    - removing console logs
    - removing not used imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React, {useState} from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import Greeter from "./components/Greeter";
 import ShoppingList from "./components/ShoppingList";
 import Product from "./models/product";
-import ShoppingListForm from "./components/ShoppingListForm"
+import ShoppingListForm from "./components/ShoppingListForm";
 
-console.log("hello");
 
 
 function App() {
-const [items, setItems] = useState<Product[]>([])
-const addItem = (product: string, qty: number) => {
-  console.log("made it to the app component");
-  console.log(product)
-  setItems([...items, {id:Date.now(), product, qty}])
-}
+  const [items, setItems] = useState<Product[]>([]);
+  const addItem = (product: string, qty: number) => {
+    setItems([...items, { id: Date.now(), product, qty }]);
+  };
   return (
     <div className="App">
       <h1>yoyoy</h1>
-      <ShoppingList items={items}/>
+      <ShoppingList items={items} />
       <ShoppingListForm onAddItem={addItem} />
     </div>
   );
